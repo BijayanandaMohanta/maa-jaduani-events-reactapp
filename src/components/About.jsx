58c2188fbaf20f394eb21ea708d15c7e17686ec6
Bijayananda Mohanta <bijayananda.dev@colourmoon.com>
2025-10-10 21:49:10 +0530

    small

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,24 +6,24 @@ function About() {
   return (
     <div className="container mt-5">
       <h2 className="text-center mb-3">About Maa Jaduani Events</h2>
-      <div className="row align-items-center">
+      <div className="row align-items-top">
         <div className="col-md-6">
           <img src={aboutImage} alt="About Us" className="img-fluid rounded" />
         </div>
         <div className="col-md-6">
-          <h3>Our Mission</h3>
+          <h5>Our Mission</h5>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec justo eu velit tincidunt
             bibendum. Sed euismod, nunc eget aliquam ultricies, nunc nisl ultricies nunc, eget aliquam
             nunc.
           </p>
-          <h3>Our Values</h3>
+          <h5>Our Values</h5>
           <ul>
             <li>Excellence</li>
             <li>Creativity</li>
             <li>Customer Satisfaction</li>
           </ul>
-          <h3>Services We Offer</h3>
+          <h5>Services We Offer</h5>
           <ul>
             <li>Event Planning</li>
             <li>Decoration</li>
